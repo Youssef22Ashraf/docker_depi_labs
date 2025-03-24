02a4a19b4ccf1e9bd2a1c5c77dba75f54f2b358a
youssef Ashraf <youssef.ashraf.elnaggar@gmail.com>
2025-03-24 04:00:16 +0200

    finishing the lab2

---
+++ lab2/Q3/react-app-dir/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React with Docker!</h1>
+      <p>This is a simple React app running inside a container with smaller image size.</p>
+    </div>
+  );
+}
+
+export default App;
